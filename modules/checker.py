8f8fc046fb417b1dbbc369400fffeb4afc715857
Mark Pitblado <mark@pitblado.me>
2025-06-13 15:58:21 -0700

    fix: add timeout to quick checker

--- modules/checker.py
+++ modules/checker.py
@@ -6,7 +6,7 @@ def is_valid_url(url: str) -> bool:
     Checks if the url responds to a ping before downloading the data.
     """
     try:
-        requests.head(url)
+        requests.head(url, timeout=3)
         return True
     except requests.ConnectionError:
         return False
