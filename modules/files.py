84db379a9eb06072e25d1497558cb667d84c4f59
Mark Pitblado <mark@pitblado.me>
2025-06-30 16:17:46 -0700

    fix: clean matching ids with excess images

--- modules/files.py
+++ modules/files.py
@@ -7,17 +7,31 @@ def read_integers_to_set(filename):
         return {int(line.strip()) for line in file}
 
 
-def clean_excess_images(n_images: int):
+def clean_excess_images(n_images: int, id_filepath="output/ids.txt"):
     """
     Reduces the number of images in the output directory down to the number requested.
     """
-    image_extensions = "*.jpg"
+    image_extensions = ["*.jpg", "*.JPG", "*.jpeg", "*.JPEG"]
     image_files = []
     for ext in image_extensions:
         image_files.extend(glob.glob(os.path.join("output", ext)))
     # Sort images by modificaiton time (no real to do, or not do this, over any other method)
     image_files.sort(key=os.path.getmtime)
-    # 3 is a magic number here, handles the license.json and the id.txt
-    while len(image_files) > n_images + 3:
-        oldest_image = image_files.pop(0)
-        os.remove(oldest_image)
+
+    # Read the ids.txt file to get the id numbers logged
+    if os.path.exists(id_filepath):
+        with open(id_filepath, "r") as f:
+            lines = f.read().splitlines()
+    else:
+        lines = []
+
+    # Remove excess images and update txt file
+
+    while len(image_files) >= n_images + 1:
+        image_path = image_files.pop(0)
+        filename = os.path.splitext(os.path.basename(image_path))[0]
+        os.remove(image_path)
+        lines = [line for line in lines if line.strip() != filename]
+
+    with open(id_filepath, "w") as f:
+        f.write("\n".join(lines))
